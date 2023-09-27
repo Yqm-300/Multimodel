480ac141d605c76a7badd836c4d3d4df392850df
Yqm <396808091@qq.com>
2023-09-27 15:48:36 +0800

    0927

--- GoogleEarthEngine/function.py
+++ GoogleEarthEngine/function.py
@@ -1,5 +1,6 @@
 import ee
 
+#ndvi计算函数，返回增加ndvi的图片
 def ndvi(image):
     ndvi = image.normalizedDifference(['B5', 'B4']).rename('NDVI')
     return image.addBands(ndvi)
@@ -7,8 +8,19 @@ def ndvi(image):
 # 创建一个函数来计算NDVI的均值
 #算的是一张图片所有像素点的平均值
 def ndvi_mean_single(image):
-    mean = image.reduceRegion(reducer=ee.Reducer.mean(),  scale=30, maxPixels=1e9)
+    mean = image.reduceRegion(reducer=ee.Reducer.mean(),  scale=30, maxPixels=1e9)#计算所有波段的平均值
     return image.set('meanNDVI', mean.get('NDVI'))
+
+#计算一张图片的平均lst
+def lst_mean_single(image):
+    mean = image.reduceRegion(reducer=ee.Reducer.mean(),  scale=30, maxPixels=1e9)#计算所有波段的平均值
+    return image.set('meanLst', mean.get('LST_Day_1km'))
+
+#从图片中获取日期和值的列表
+def ndvi_dates_values(ndvi_data):
+    dates = [ee.Date(image['properties']['system:time_start']).format('YYYY-MM-dd').getInfo() for image in ndvi_data['features']]
+    ndvi_values = [image['properties']['meanNDVI'] for image in ndvi_data['features']]
+    return dates, ndvi_values
 
 #对两个列表的数据去重（取平均）
 from collections import defaultdict
@@ -33,3 +45,4 @@ def remove_duplicates(Dates,Values):
     sorted_data = sorted(zip(unique_dates, average_ndvi_values), key=lambda x: x[0])
     sorted_dates, sorted_values = zip(*sorted_data)
     return sorted_dates,sorted_values
+
